af32e20ccd1d0fcac91572cb5087e8f6f46f99f3
Norbert <soczkers@gmail.com>
2019-04-05 21:57:51 +0100

    test fix

--- pinreader.py
+++ pinreader.py
@@ -1,3 +1,4 @@
+import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
@@ -6,7 +7,8 @@ def my_callback(channel):
 
 for i in [4,17]:
     GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(i, GPIO.RISING)
-    GPIO.add_event_callback(channel, my_callback, bouncetime=2000)
+    GPIO.add_event_detect(i, GPIO.RISING, callback=my_callback, bouncetime=2000)
     print(i,"done")
+
+while True:
+    time.sleep(1)
