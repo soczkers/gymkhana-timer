82d1b315acc54bb0918320c47e6122b6e4271dfa
Norbert Targosz <ntargosz@egnyte.com>
2019-04-05 23:31:01 +0200

    channel reader test

--- pinreader.py
+++ pinreader.py
@@ -1,11 +1,14 @@
 import time
 import requests
 import RPi.GPIO as GPIO
+TARGET="http://localhost:9090"
+TARGET="http://requestbin.fullcontact.com/1blz63q1"
+
 GPIO.setmode(GPIO.BCM)
 
 def my_callback(channel):
     payload = {'channel': channel, 'time': time.time()}
-    print(requests.post("localhost:9090", data=payload))
+    print(requests.post(TARGET, data=payload))
 
 for i in [4,17]:
     GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
