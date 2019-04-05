bb8e1695abe2162494c2cfa66a4d462727c56656
Norbert <soczkers@gmail.com>
2019-04-05 22:47:37 +0100

    test fix

--- pinreader.py
+++ pinreader.py
@@ -1,19 +1,18 @@
 import time
 import requests
 import RPi.GPIO as GPIO
-TARGET="http://localhost:9090"
-TARGET="http://requestbin.fullcontact.com/1blz63q1"
+#TARGET="http://localhost:9090"
+TARGET="https://httpbin.org/post"
 
 GPIO.setmode(GPIO.BCM)
 
 def my_callback(channel):
     payload = {'channel': channel, 'time': time.time()}
-    print(requests.post(TARGET, data=payload))
+    print(requests.post(TARGET, data=payload).text)
 
 for i in [4,17]:
     GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.add_event_detect(i, GPIO.RISING, callback=my_callback, bouncetime=2000)
-    print(i,"done")
 
 while True:
     time.sleep(1)
