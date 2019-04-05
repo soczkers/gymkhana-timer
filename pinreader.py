122a919f29b92c5fdd163ed5058685bf85e23d2e
Norbert <soczkers@gmail.com>
2019-04-05 22:23:47 +0100

    test fix

--- pinreader.py
+++ pinreader.py
@@ -5,7 +5,7 @@ GPIO.setmode(GPIO.BCM)
 
 def my_callback(channel):
     payload = {'channel': channel, 'time': time.time()}
-    print(requests.post("localhost:9090", data=payload))
+    print(requests.post("http://localhost:9090", data=payload))
 
 for i in [4,17]:
     GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
